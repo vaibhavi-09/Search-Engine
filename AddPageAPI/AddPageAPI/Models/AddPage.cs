06dc8eb53429281defee1418762985690e0c25d2
vaibhavi-09 <62421153+vaibhavi-09@users.noreply.github.com>
2020-04-04 11:40:42 +0530

    Add files via upload

---
+++ AddPageAPI/AddPageAPI/Models/AddPage.cs
@@ -0,0 +1,10 @@
+namespace AddPageAPI.Models{
+
+    public class AddPage{
+        public string Title { get; set;}
+        public string Tags { get; set;}
+        public string Description { get; set;}
+        public string Like { get; set;}
+        
+         }
+}
