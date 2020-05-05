bd6190548e3b7d5865e0e7d25728a6e6d85a41e9
vaibhavi-09 <62421153+vaibhavi-09@users.noreply.github.com>
2020-05-05 19:36:02 +0530

    App.js with tags functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
 import React from 'react';
 import axios from 'axios';
-import { Form,Header ,Segment,Grid,Label,List,Button,Menu} from 'semantic-ui-react';
+import { Form,Header ,Segment,Grid,Label,List,Button,Menu,Divider} from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import TagsInput from 'react-tagsinput';
+import './index.css';
+import { SketchPicker } from 'react-color';
+import 'react-tagsinput/react-tagsinput.css'
+
 
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      tags: '',
-      description: '',
+      tags: [],
+     description: '',
     };
     
   }
 
+  handleChange(tags) {
+    this.setState({tags})
+  }
   onChange = (e) => {
   
     this.setState({ [e.target.name]: e.target.value });
   }
-  onSubmit =  (e) => {
+  onSubmit =  (e) => 
+  {
     e.preventDefault();
     // get our form data out of state
     const { title, tags, description } = this.state;
-  
+    const searchUrl='https://localhost:5001/api/AddPage/AddPageFunc'
     let sendData={'title':title,'tags':tags,'description':description}
     let axiosConfig ={
       headers:{
         'Content-Type' : 'application/json'
       }
     };
-    axios.post('http://localhost:5000/api/AddPage/AddPageFunc',sendData,axiosConfig).then(response => console.log(response))
+    axios.post(searchUrl,sendData,axiosConfig).then(response => console.log(response))
     .catch(error => console.log(error));
 
   
   }
  render() {
-  const { title, tags, description } = this.state;
+  const {title, tags, description } = this.state;
   return (
   <div>
     <Menu inverted color='blue'>
@@ -43,21 +52,26 @@ class MyForm extends React.Component {
     <Menu.Item name='Results'/>
     </Menu>
     
-    <Segment inverted color='violet'>Add Page</Segment>
-   
+    <Segment inverted color='teal'>Add Page</Segment>
+   <Segment>
+   <div class="ui internally celled grid">
+   <div class="ten wide column">
   <Form  class="ui form" onSubmit={this.onSubmit}>
   <div class="field">
-  <Label>Title <input type="text" name="title" value={title} onChange={this.onChange}/></Label>
-  </div>
-  <div class="field">
-  <Label>Tags  <input type="text" name="tags" value={tags} onChange={this.onChange}/></Label>
+  <Label>Title <input type="text" name="title" value={this.state.title} onChange={this.onChange}/></Label>
   </div>
   <div class="field">
   <Label>Description </Label>
-  <textarea name="description" value={description} onChange={this.onChange} ></textarea>
+  <textarea name="description" value={this.state.description} onChange={this.onChange} ></textarea>
   </div>
   <Button color='green' type="submit"  >Submit</Button>
   </Form>
+  </div>
+  <div class="six wide column">
+      <Label>Tags<TagsInput addKeys={[9, 13, 32, 188]} name="tags"  value={this.state.tags} onChange={this.handleChange.bind(this)}/></Label>
+      </div>
+  </div>
+  </Segment>
   
   <Segment inverted color='blue'>
   <Grid divided inverted stackable>
